bd61104f2cbfd7f121c3827fc374bef78f8416f9
quick007 <69743171+quick007@users.noreply.github.com>
2025-04-05 14:45:47 -0700

    Chicken jockey - Bloxs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const geistSans = Inter({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`bg-[#142F35] min-h-screen flex flex-col text-white ${geistSans.className}`}>
-      <nav className="rounded-xl bg-white/10 border-2 border-white/20 px-4 py-4 mt-4 mx-8 flex justify-between items-center">
+      <nav className="rounded-xl bg-white/10 border-2 border-white/20 px-4 py-2 mt-4 mx-8 flex justify-between items-center">
         <div className="flex gap-4 items-center text-lg font-bold tracking-wide">
           <img src="/logo.png" className="size-8" />
           <p>Signlingo</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="flex h-[70dvh] flex-col items-center justify-center">
+      <div className="flex flex-col items-center motion-preset-slide-up motion-duration-1500-md gap-6">
+        <h1 className="text-5xl/14 font-semibold max-w-lg text-center bg-clip-text text-transparent bg-linear-to-b from-white to-white/70">
+          Master ASL The Engaging Way
+        </h1>
+        <p className="text-white/80 font-medium max-w-md text-center">
+          Learn American Sign Language by breaking up the task into short daily
+          exersizes, gamified for smth.
+        </p>
+      </div>
+      <button className="mt-8 rounded-lg bg-cyan-800 font-medium text-lg w-72 h-10 border-b-3 ">
+        Get Started
+      </button>
     </div>
   );
 }
