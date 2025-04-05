7726911ee9ce274eba248fb195a6a73af43edb65
quick007 <69743171+quick007@users.noreply.github.com>
2025-04-05 15:01:55 -0700

    work on homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-
-const geistSans = Inter({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`bg-[#142F35] min-h-screen flex flex-col text-white ${geistSans.className}`}>
+    <main className={`bg-[#142F35] min-h-screen flex flex-col text-white inter`}>
       <nav className="rounded-xl bg-white/10 border-2 border-white/20 px-4 py-2 mt-4 mx-8 flex justify-between items-center">
         <div className="flex gap-4 items-center text-lg font-bold tracking-wide">
           <img src="/logo.png" className="size-8" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { cn } from "@/utils/cn";
+
 export default function Home() {
   return (
     <div className="flex h-[70dvh] flex-col items-center justify-center">
-      <div className="flex flex-col items-center motion-preset-slide-up motion-duration-1500-md gap-6">
+      <div className="flex flex-col items-center motion-preset-slide-up motion-duration-1500 gap-6">
         <h1 className="text-5xl/14 font-semibold max-w-lg text-center bg-clip-text text-transparent bg-linear-to-b from-white to-white/70">
           Master ASL The Engaging Way
         </h1>
@@ -10,7 +12,13 @@ export default function Home() {
           exersizes, gamified for smth.
         </p>
       </div>
-      <button className="mt-8 rounded-lg bg-cyan-800 font-medium text-lg w-72 h-10 border-b-3 ">
+      <button
+        className={cn(
+          "mt-8 rounded-xl bg-cyan-800 font-medium text-lg w-72 h-12 border-b-4 border-cyan-900",
+          "motion-delay-650 motion-preset-slide-up motion-duration-1000",
+          "transition-all hover:focus:border-b-0 hover:focus:translate-y-2 hover:focus:scale-y-92"
+        )}
+      >
         Get Started
       </button>
     </div>
