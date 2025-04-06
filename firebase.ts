a7e5f242b64f17d2e5649dd60696738c80f9eddd
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2025-04-05 21:07:11 -0500

    Bottom text

--- firebase.ts
+++ firebase.ts
@@ -6,19 +6,16 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 
-// I know that this isn't a good practice with hardcoding credentials into the code
-// But we (I) don't care enough to configure an env because of the following:
-// 1. Firebase credentials are not sensitive https://firebase.google.com/docs/projects/api-keys#general-info 
-// 2. This is a hackathon, as long as these aren't admin creds it's *probably* fine
-// 3. These are shipped to the client anyways, so not like it's a secret
-// - Bloxs
+// I have been informed that I have been called out for copium (even though I disagree (I wonder why :trollface:))
+// so I took the effort to move these to an env even though they're public
+// Github and deno deploy, please add uploading .env files - Bloxs
 const firebaseConfig = {
-  apiKey: "AIzaSyA3wFVNF4olPI5hfeWzjwb30qS7WSyxC2k",
-  authDomain: "signlingo-793a1.firebaseapp.com",
-  projectId: "signlingo-793a1",
-  storageBucket: "signlingo-793a1.firebasestorage.app",
-  messagingSenderId: "728737982745",
-  appId: "1:728737982745:web:324842fd0355cb35fbfd88"
+	apiKey: process.env.NODE_PUBLIC_apiKey,
+	authDomain: process.env.NODE_PUBLIC_authDomain,
+	projectId: process.env.NODE_PUBLIC_projectId,
+	storageBucket: process.env.NODE_PUBLIC_storageBucket,
+	messagingSenderId: process.env.NODE_PUBLIC_messagingSenderId,
+	appId: process.env.NODE_PUBLIC_appId,
 };
 
 // Initialize Firebase
